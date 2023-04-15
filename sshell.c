68ec5495c3e8d31367e83c8ac7b2465aaa9e783e
fangyunh <726442030@qq.com>
2023-04-15 00:38:50 -0400

    Change parsing from array to linked list

--- sshell.c
+++ sshell.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdbool.h>
 #include <fcntl.h>
 #include <sys/wait.h>
 
@@ -9,8 +10,92 @@
 #define CMDLINE_MAXARGNUM 16
 #define CMDLINE_MAXTOKLEN 32
 
-char** parseCmdInArr(char *cmd);
-void freeArray(char** arr, int elementNum);
+/* Structure for parsing commands */
+struct inputCmd {
+    char *cmdStr;
+    struct inputCmd *next;
+};
+
+/* free the allocated space */
+void freeLinkedList(struct inputCmd *head) {
+    struct inputCmd *ptr = head;
+    while (ptr) {
+        struct inputCmd *node = ptr->next;
+        free(ptr->cmdStr);
+        free(ptr);
+        ptr = node;
+    }
+}
+
+/* Parse the cmds in array (fangyunh) */
+struct inputCmd* parseCmdInList(char* cmd) {
+    //char** commands = (char**) calloc(CMDLINE_MAXARGNUM, sizeof(char*));
+    int cursor = 0;
+    char* token = (char*) calloc(CMDLINE_MAXTOKLEN, sizeof(char));
+    char* pos;
+    int count = 0;
+    struct inputCmd *head = NULL;
+    struct inputCmd *tail = NULL;
+    if (cmd == NULL || *cmd == '\0') {
+        fprintf(stderr, "Empty command input.\n");
+        free(token);
+        return NULL;
+    }
+    while (1) {
+        if (count > CMDLINE_MAXARGNUM) {
+            fprintf(stderr, "Command exceeds the limit of number of tokens.\n");
+            free(token);
+            //freeLinkedList(head);
+            return NULL;
+        }
+
+        sscanf(cmd + cursor, "%[^ ]", token);
+        if (strlen(token) > CMDLINE_MAXTOKLEN) {
+            fprintf(stderr, "Command exceeds the limit of token max length\n");
+            free(token);
+            //freeLinkedList(head);
+            return NULL;
+        }
+        if (!token) {
+            break;
+        }
+
+        struct inputCmd *newNode = (struct inputCmd*) calloc(1, sizeof(struct inputCmd));
+        newNode->cmdStr = (char *) calloc(strlen(token) + 1, sizeof(char));
+        if (!newNode->cmdStr) {
+            fprintf(stderr, "newNode memory allocation fail");
+            free(newNode);
+            //freeLinkedList(head);
+            return NULL;
+        }
+        if (strcmp(token, "\0") != 0) {
+            strcpy(newNode->cmdStr, token);
+            newNode->next = NULL;
+
+            if (!head) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail->next = newNode;
+                tail = newNode;
+            }
+            count++;
+        }
+
+
+        pos = strchr(cmd + cursor, ' ');
+        while (pos != NULL && *(pos+1) == ' ') {
+            pos++;
+        }
+        if (pos == NULL) {
+            break;
+        }
+        cursor = pos - cmd + 1;
+    }
+    free(token);
+
+    return head;
+}
 
 int main(void)
 {
@@ -20,7 +105,7 @@ int main(void)
             char *nl;
             int retval;
             pid_t pid;
-            char** commandArr;
+            struct inputCmd *commandArr;
 
             /* Print prompt */
             printf("sshell@ucd$ ");
@@ -45,9 +130,14 @@ int main(void)
                 *nl = '\0';
 
             /* Parse the commands in an array */
-            commandArr = parseCmdInArr(cmd);
-            if (commandArr == NULL) {
-                continue;
+            commandArr = parseCmdInList(cmd);
+
+            struct inputCmd *hello = commandArr;
+            int a = 0;
+            while (hello) {
+                printf("The %dth is %s\n", a, hello->cmdStr);
+                hello = hello->next;
+                a++;
             }
 
 
@@ -74,60 +164,8 @@ int main(void)
                 fprintf(stderr, "+ completed '%s': [%d]\n",
                         cmd, WEXITSTATUS(retval));
             }
-            freeArray(commandArr, CMDLINE_MAXARGNUM);
+            freeLinkedList(commandArr);
         }
 
         return EXIT_SUCCESS;
 }
-
-/* Parse the cmds in array (fangyunh) */
-char** parseCmdInArr(char* cmd) {
-    char** commands = (char**) calloc(CMDLINE_MAXARGNUM, sizeof(char*));
-    int cursor = 0;
-    char* token = (char*) calloc(CMDLINE_MAXTOKLEN, sizeof(char));
-    char* pos;
-    int count = 0;
-
-    while (cmd != NULL) {
-        if (count > CMDLINE_MAXARGNUM) {
-            fprintf(stderr, "Command exceeds the limit of number of tokens.\n");
-            free(token);
-            freeArray(commands, count);
-            return NULL;
-        }
-
-        sscanf(cmd + cursor, "%[^ ]", token);
-        if (strlen(token) > CMDLINE_MAXTOKLEN) {
-            fprintf(stderr, "Command exceeds the limit of token max length\n");
-            free(token);
-            freeArray(commands, count);
-            return NULL;
-        }
-        commands[count] = (char*) calloc(strlen(token) + 1, sizeof(char));
-        strcpy(commands[count], token);
-        count++;
-
-        pos = strchr(cmd + cursor, ' ');
-        while (pos != NULL && *(pos+1) == ' ') {
-            pos++;
-        }
-        if (pos == NULL) {
-            break;
-        }
-        cursor++;
-        cursor = pos - cmd + 1;
-    }
-    free(token);
-
-    return commands;
-}
-
-/* free the allocated space */
-void freeArray(char** arr, int elementNum) {
-    for (int i = 0; i < elementNum; i++) {
-        free(arr[i]);
-    }
-    free(arr);
-}
-
-
