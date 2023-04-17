815f46455977d8a3a6459608cff81bf0daef63bf
fangyunh <726442030@qq.com>
2023-04-17 03:49:27 -0400

    Add pwd and output redirection module, output redirection currently only split the command by > in linked lists. rmvLast() is used to delete the duplicated element in the first command.

--- sshell.c
+++ sshell.c
@@ -9,17 +9,21 @@
 #define CMDLINE_MAXCHAR 512
 #define CMDLINE_MAXARGNUM 16
 #define CMDLINE_MAXTOKLEN 32
+#define MAX_BUFFER_SIZE 256
 
 /* Structure for parsing commands */
-struct inputCmd {
+struct inputCmd
+{
     char *cmdStr;
     struct inputCmd *next;
 };
 
 /* free the allocated space */
-void freeLinkedList(struct inputCmd *head) {
+void freeLinkedList(struct inputCmd *head)
+{
     struct inputCmd *ptr = head;
-    while (ptr) {
+    while (ptr)
+    {
         struct inputCmd *node = ptr->next;
         free(ptr->cmdStr);
         free(ptr);
@@ -28,66 +32,76 @@ void freeLinkedList(struct inputCmd *head) {
 }
 
 /* Parse the cmds in linked-list (fangyunh) */
-struct inputCmd* parseCmdInList(char* cmd) {
-    //char** commands = (char**) calloc(CMDLINE_MAXARGNUM, sizeof(char*));
+struct inputCmd *parseCmdInList(char *cmd)
+{
+    // char** commands = (char**) calloc(CMDLINE_MAXARGNUM, sizeof(char*));
     int cursor = 0;
-    char* token = (char*) calloc(CMDLINE_MAXTOKLEN, sizeof(char));
-    char* pos;
+    char *token = (char *)calloc(CMDLINE_MAXTOKLEN, sizeof(char));
+    char *pos;
     int count = 0;
     struct inputCmd *head = NULL;
     struct inputCmd *tail = NULL;
-    if (cmd == NULL || *cmd == '\0') {
+    if (cmd == NULL || *cmd == '\0')
+    {
         fprintf(stderr, "Empty command input.\n");
         free(token);
         return NULL;
     }
-    while (1) {
-        if (count > CMDLINE_MAXARGNUM) {
+    while (1)
+    {
+        if (count > CMDLINE_MAXARGNUM)
+        {
             fprintf(stderr, "Command exceeds the limit of number of tokens.\n");
             free(token);
-            //freeLinkedList(head);
             return NULL;
         }
 
         sscanf(cmd + cursor, "%[^ ]", token);
-        if (strlen(token) > CMDLINE_MAXTOKLEN) {
+        if (strlen(token) > CMDLINE_MAXTOKLEN)
+        {
             fprintf(stderr, "Command exceeds the limit of token max length\n");
             free(token);
-            //freeLinkedList(head);
             return NULL;
         }
-        if (!token) {
+        if (!token)
+        {
             break;
         }
 
-        struct inputCmd *newNode = (struct inputCmd*) calloc(1, sizeof(struct inputCmd));
-        newNode->cmdStr = (char *) calloc(strlen(token) + 1, sizeof(char));
-        if (!newNode->cmdStr) {
+        struct inputCmd *newNode = (struct inputCmd *)calloc(1, sizeof(struct inputCmd));
+        newNode->cmdStr = (char *)calloc(strlen(token) + 1, sizeof(char));
+        if (!newNode->cmdStr)
+        {
             fprintf(stderr, "newNode memory allocation fail");
             free(newNode);
-            //freeLinkedList(head);
+            // freeLinkedList(head);
             return NULL;
         }
-        if (strcmp(token, "\0") != 0) {
+        if (strcmp(token, "\0") != 0)
+        {
             strcpy(newNode->cmdStr, token);
             newNode->next = NULL;
 
-            if (!head) {
+            if (!head)
+            {
                 head = newNode;
                 tail = newNode;
-            } else {
+            }
+            else
+            {
                 tail->next = newNode;
                 tail = newNode;
             }
             count++;
         }
 
-
         pos = strchr(cmd + cursor, ' ');
-        while (pos != NULL && *(pos+1) == ' ') {
+        while (pos != NULL && *(pos + 1) == ' ')
+        {
             pos++;
         }
-        if (pos == NULL) {
+        if (pos == NULL)
+        {
             break;
         }
         cursor = pos - cmd + 1;
@@ -97,22 +111,69 @@ struct inputCmd* parseCmdInList(char* cmd) {
     return head;
 }
 
+/* Remove the last element in the linked list */
+void rmvLast(struct inputCmd *head)
+{
+    while (head->next->next != NULL)
+    {
+        head = head->next;
+    }
+    struct inputCmd *tail = head;
+    tail = tail->next;
+    head->next = NULL;
+    freeLinkedList(tail);
+}
+
+/* Output Redirection */
+void outputRedirection(char *cmd)
+{
+    char *firstCmd = strtok(cmd, ">");
+    struct inputCmd *firstHead = parseCmdInList(firstCmd);
+    char *secondCmd = strtok(NULL, ">");
+    struct inputCmd *secondHead = parseCmdInList(secondCmd);
+    rmvLast(firstHead);
+    
+    freeLinkedList(secondHead);
+    freeLinkedList(firstHead);
+}
+
+/* PWD built-in function */
+void builtinPWD()
+{
+    char buffer[MAX_BUFFER_SIZE];
+
+    if (getcwd(buffer, sizeof(buffer)) != NULL)
+    {
+        printf("%s\n", buffer);
+    }
+    else
+    {
+        fprintf(stderr, "PWD error");
+    }
+}
+
 /* cd built-in function */
-void builtinCd (struct inputCmd *head) {
+void builtinCd(struct inputCmd *head)
+{
     struct inputCmd *filePath = head->next;
     int status = chdir(filePath->cmdStr);
-    if (status == -1) {
+    if (status == -1)
+    {
         fprintf(stderr, "cd error");
     }
 }
 
-
 /* Determine and execute bulit-in functions */
-bool builtinFunctions(struct inputCmd *head) {
-    if (!strcmp(head->cmdStr, "cd")) {
+bool builtinFunctions(struct inputCmd *head)
+{
+    if (!strcmp(head->cmdStr, "cd"))
+    {
         builtinCd(head);
         return true;
-    } else if (!strcmp(head->cmdStr, "pwd")) {
+    }
+    else if (!strcmp(head->cmdStr, "pwd"))
+    {
+        builtinPWD();
         return true;
     }
     return false;
@@ -120,74 +181,91 @@ bool builtinFunctions(struct inputCmd *head) {
 
 int main(void)
 {
-        char cmd[CMDLINE_MAXCHAR];
+    char cmd[CMDLINE_MAXCHAR];
 
-        while (1) {
-            char *nl;
-            int retval;
-            pid_t pid;
-            struct inputCmd *commandList;
-            bool builtinFlag = false;
+    while (1)
+    {
+        char *nl;
+        int retval;
+        pid_t pid;
+        struct inputCmd *commandList;
+        bool builtinFlag = false;
 
-            /* Print prompt */
-            printf("sshell@ucd$ ");
-            fflush(stdout);
+        /* Print prompt */
+        printf("sshell@ucd$ ");
+        fflush(stdout);
 
-            /* Get command line */
-            fgets(cmd, CMDLINE_MAXCHAR, stdin);
-            if (cmd[strlen(cmd) - 1] != '\n') {
-                fprintf(stderr, "Command exceeds the limit.\n");
-                continue;
-            }
-
-            /* Print command line if stdin is not provided by terminal */
-            if (!isatty(STDIN_FILENO)) {
-                printf("%s", cmd);
-                fflush(stdout);
-            }
-
-            /* Remove trailing newline from command line */
-            nl = strchr(cmd, '\n');
-            if (nl)
-                *nl = '\0';
-
-            /* Parse the commands in an array */
-            commandList = parseCmdInList(cmd);
-
-            /* Builtin command */
-            if (!strcmp(cmd, "exit")) {
-                fprintf(stderr, "Bye...\n");
-                fprintf(stderr, "+ completed '%s': [%d]\n",
-                        cmd, 0);
-                break;
-            }
-            builtinFlag = builtinFunctions(commandList);
-            if (builtinFlag) {
-                fprintf(stderr, "+ completed '%s': [%d]\n",
-                        cmd, 0);
-                continue;
-            }
-
-            /* Regular command, $PATH environment commands*/
-
-            pid = fork();
-            if (pid < 0) {
-                fprintf(stderr, "fork error");
-                exit(EXIT_FAILURE);
-            } else if (pid == 0) {
-                // Child process
-                char* instru[] = {"sh", "-c", cmd, NULL };
-                execvp("sh", instru);
-                fprintf(stderr, "execvp error");
-                exit(EXIT_FAILURE);
-            } else {
-                // Parent process
-                wait(&retval);
-                fprintf(stderr, "+ completed '%s': [%d]\n",
-                        cmd, WEXITSTATUS(retval));
-            }
-            freeLinkedList(commandList);
+        /* Get command line */
+        fgets(cmd, CMDLINE_MAXCHAR, stdin);
+        if (cmd[strlen(cmd) - 1] != '\n')
+        {
+            fprintf(stderr, "Command exceeds the limit.\n");
+            continue;
         }
 
-        return EXIT_SUCCESS;
+        /* Print command line if stdin is not provided by terminal */
+        if (!isatty(STDIN_FILENO))
+        {
+            printf("%s", cmd);
+            fflush(stdout);
+        }
+
+        /* Remove trailing newline from command line */
+        nl = strchr(cmd, '\n');
+        if (nl)
+            *nl = '\0';
+
+        /* Detect output redirection */
+        char *hasRedirect = strchr(cmd, '>');
+        if (hasRedirect)
+        {
+            outputRedirection(cmd);
+        }
+
+        /* Parse the commands in a Linked list */
+        commandList = parseCmdInList(cmd);
+
+        /* Builtin command */
+        if (!strcmp(cmd, "exit"))
+        {
+            fprintf(stderr, "Bye...\n");
+            fprintf(stderr, "+ completed '%s': [%d]\n",
+                    cmd, 0);
+            break;
+        }
+        builtinFlag = builtinFunctions(commandList);
+        if (builtinFlag)
+        {
+            fprintf(stderr, "+ completed '%s': [%d]\n",
+                    cmd, 0);
+            continue;
+        }
+
+        /* Regular command, $PATH environment commands*/
+        pid = fork();
+        if (pid < 0)
+        {
+            fprintf(stderr, "fork error");
+            exit(EXIT_FAILURE);
+        }
+        else if (pid == 0)
+        {
+            // Child process
+            char *instru[] = {"sh", "-c", cmd, NULL};
+            execvp("sh", instru);
+            fprintf(stderr, "execvp error");
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            // Parent process
+            wait(&retval);
+            fprintf(stderr, "+ completed '%s': [%d]\n",
+                    cmd, WEXITSTATUS(retval));
+        }
+
+        freeLinkedList(commandList);
+    }
+
+    return EXIT_SUCCESS;
 }
